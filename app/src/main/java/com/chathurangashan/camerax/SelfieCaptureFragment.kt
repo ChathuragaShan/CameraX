b9eaaeedea88bd180f0dc1d882dc41f6bf6cf8a7
ChathuragaShan <chathurangashan@gmail.ocm>
2022-08-02 23:39:26 +0530

    completed camera preview.

--- app/src/main/java/com/chathurangashan/camerax/SelfieCaptureFragment.kt
+++ app/src/main/java/com/chathurangashan/camerax/SelfieCaptureFragment.kt
@@ -2,14 +2,16 @@ package com.chathurangashan.camerax
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.Surface
 import android.view.View
-import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
@@ -17,11 +19,10 @@ import androidx.navigation.Navigation
 import com.chathurangashan.camerax.databinding.FragmentSelfieCaptureBinding
 import com.google.android.material.snackbar.Snackbar
 
-
 class SelfieCaptureFragment : Fragment(R.layout.fragment_selfie_capture) {
 
     private lateinit var viewBinding: FragmentSelfieCaptureBinding
-    protected lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<Array<String>>
     private val navigationController: NavController by lazy {
         Navigation.findNavController(requireView())
     }
@@ -37,12 +38,13 @@ class SelfieCaptureFragment : Fragment(R.layout.fragment_selfie_capture) {
 
     private fun initialization() {
 
-        viewBinding.root.waitForLayout{
+        viewBinding.root.waitForLayout {
 
             viewBinding.cameraCutoutView.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
 
             val circleCutoutRadius = resources.getDimension(R.dimen.circle_cut_out_radius)
-            val marginFromCircleCutout = resources.getDimension(R.dimen.content_margin_from_circle_cutout)
+            val marginFromCircleCutout =
+                resources.getDimension(R.dimen.content_margin_from_circle_cutout)
             val marginToContent = circleCutoutRadius + marginFromCircleCutout
 
             val descriptionLayoutParams = viewBinding.descriptionTextView.layoutParams
@@ -58,40 +60,72 @@ class SelfieCaptureFragment : Fragment(R.layout.fragment_selfie_capture) {
             viewBinding.dynamicalyAlineGroup.visibility = View.VISIBLE
         }
 
-        requestPermissionLauncher = registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                viewBinding.cameraView.visibility = View.VISIBLE
-                //setupCameraIdWithView()
-            } else {
-                // Permission request was denied.
-                Snackbar.make(
-                    viewBinding.root,
-                    R.string.camera_permission_denied, Snackbar.LENGTH_SHORT
-                ).setAction(R.string.ok) { navigationController.navigateUp() }.show()
-
-            }
-        }
-
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
-            == PackageManager.PERMISSION_GRANTED) {
-            //setupCameraIdWithView()
-        } else {
-            viewBinding.cameraView.visibility = View.GONE
-            requestCameraPermission()
-        }
+        checkCameraPermission()
 
         viewBinding.cameraCutoutView.waitForLayout {
             cameraCutoutPivotX = viewBinding.cameraCutoutView.pivotX
             cameraCutoutPivotY = viewBinding.cameraCutoutView.pivotY
         }
+
+        requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+
+            it.entries.forEach { permission ->
+
+                when (permission.key) {
+
+                    Manifest.permission.CAMERA -> {
+
+                        if (!permission.value) {
+                            Snackbar.make(
+                                viewBinding.root,
+                                R.string.camera_permission_denied, Snackbar.LENGTH_SHORT
+                            ).setAction(R.string.ok) {
+                                requireActivity().finish()
+                            }.show()
+                        }
+                    }
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE -> {
+
+                        if (!permission.value) {
+                            Snackbar.make(
+                                viewBinding.root,
+                                R.string.write_external_storage_denied, Snackbar.LENGTH_SHORT
+                            ).setAction(R.string.ok) {
+                                requireActivity().finish()
+                            }.show()
+                        }
+                    }
+                }
+            }
+
+            checkCameraPermission()
+        }
+
     }
 
-    private fun onClickCameraFabButton(){
+    private fun onClickCameraFabButton() {
 
         viewBinding.cameraFabButton.setOnClickListener {
             navigationController.navigate(R.id.to_selfie_preview)
+        }
+    }
+
+    /**
+     * This function is responsible for checking all the permission to start camera. if all permissions
+     * are fulfilled it will start the camera preview.
+     */
+    private fun checkCameraPermission() {
+
+        val allPermissionStatus = REQUIRED_PERMISSIONS.all {
+            ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+        }
+
+        if (allPermissionStatus) {
+            startCamera()
+        } else {
+            requestCameraPermission()
         }
     }
 
@@ -100,23 +134,70 @@ class SelfieCaptureFragment : Fragment(R.layout.fragment_selfie_capture) {
      */
     private fun requestCameraPermission() {
 
-        if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
-            Snackbar.make(viewBinding.root, R.string.camera_access_required, Snackbar.LENGTH_INDEFINITE)
+        if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) ||
+            shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        ) {
+            Snackbar.make(
+                viewBinding.root,
+                R.string.camera_access_required,
+                Snackbar.LENGTH_INDEFINITE
+            )
                 .setAction(R.string.ok) {
-                    requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+                    requestPermissionLauncher.launch(REQUIRED_PERMISSIONS)
                 }.show()
+
         } else {
-            // You can directly ask for the permission.
             Snackbar.make(
                 viewBinding.root,
                 R.string.camera_permission_not_available,
                 Snackbar.LENGTH_INDEFINITE
             )
                 .setAction(R.string.ok) {
-                    requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+                    requestPermissionLauncher.launch(REQUIRED_PERMISSIONS)
                 }.show()
         }
+    }
+
+    private fun startCamera() {
+
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewBinding.cameraView.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview
+                )
+
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(requireContext()))
 
     }
 
+    companion object {
+
+        private val TAG = SelfieCaptureFragment::class.simpleName
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf(
+                Manifest.permission.CAMERA
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
 }
